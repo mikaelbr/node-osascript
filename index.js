23e8ca0543be8a7c27e4555976965a99c420e0ed
Mikael Brevik <mikaelbre@gmail.com>
2014-09-18 20:58:51 +0200

    Refactors and adds error handling

--- index.js
+++ index.js
@@ -1,84 +1,74 @@
 var spawn = require('duplex-child-process').spawn;
 var fs = require('fs');
-var osascript = spawn('osascript')//, ['-l', 'JavaScript']);
+var osascript = spawn('osascript');
 
 var noFileMsg = 'You need to specify filename';
-var noInputMsg = 'You pass text to evaluate';
+var noInputMsg = 'You need to pass text to evaluate';
 
 module.exports = function (opts) {
   return getSpawn(opts);
 };
 
 module.exports.file = function (file, opts, cb) {
-  if (!file && cb === void 0) {
-    throw Error(noFileMsg);
-  }
-
   if (typeof opts === 'function') {
     cb = opts;
     opts = {};
   }
-
-  if (!file) {
-    return cb(new Error(noFileMsg));
-  }
+  if(!validateInput(file, cb, noFileMsg)) return;
 
   var stream = fs.createReadStream(file).pipe(getSpawn(opts));
-
-  if (cb === void 0) {
-    return stream;
-  }
-
-  var buffers = [];
-  stream.on('data', function(buffer) {
-    buffers.push(buffer);
-  });
-
-  stream.on('end', function() {
-    var buffered = Buffer.concat(buffers);
-    return cb(null, buffered.toString());
-  });
+  if (cb) bufferStream(stream, cb);
 
   return stream;
 };
 
 module.exports.eval = function (text, opts, cb) {
-  if (!text && cb === void 0) {
-    throw Error(noFileMsg);
-  }
-
   if (typeof opts === 'function') {
     cb = opts;
     opts = {};
   }
-
-  if (!text) {
-    return cb(new Error(noInputMsg));
-  }
+  if (!validateInput(text, cb, noInputMsg)) return;
 
   var stream = getSpawn(opts);
+  if (cb) bufferStream(stream, cb);
 
-  if (cb === void 0) {
-    stream.write(text);
-    stream.end();
-    return stream;
+  stream.write(text);
+  stream.end();
+  return stream;
+};
+
+function validateInput (input, cb, msg) {
+  if (!input && cb === void 0) {
+    throw Error(msg);
   }
 
-  var buffers = [];
+  if(!input) {
+    return cb(new Error(msg));
+  }
+
+  return true;
+}
+
+function bufferStream (stream, cb) {
+  var buffers = [], inError;
+
   stream.on('data', function(buffer) {
     buffers.push(buffer);
   });
 
   stream.on('end', function() {
     var buffered = Buffer.concat(buffers);
+    if (inError) return;
+
     return cb(null, buffered.toString());
   });
 
-  stream.write(text);
-  stream.end();
-
+  stream.on('error', function(err) {
+    inError = true;
+    return cb(err);
+  });
   return stream;
-};
+}
 
 function getSpawn (opts) {
   return spawn('osascript', argify(opts));
