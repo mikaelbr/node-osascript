88eb3649a11d9304ba86446578d5b06840d52e27
Mikael Brevik <mikaelbre@gmail.com>
2014-09-19 07:56:53 +0200

    Changes eval example to use output.

--- examples/eval.js
+++ examples/eval.js
@@ -1,12 +1,13 @@
 var osascript = require('../').eval;
 
-var script = 'display dialog "What should I do?" buttons {"Go home", "Work", "Nothing"}\n' +
-  'set DlogResult to result\n' +
-  'return result';
+var script = 'get volume settings';
 
 osascript(script, { type: 'AppleScript' }).pipe(process.stdout);
+// = output volume:44, input volume:75, alert volume:100, output muted:true
+
 
 // With callback
 osascript(script, { type: 'AppleScript' }, function (err, data) {
-  console.log("Data:", data);
+  console.log(data);
+  // = output volume:44, input volume:75, alert volume:100, output muted:true
 });
